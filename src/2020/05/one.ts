e5a4a5b4783c84cf50e3d94ad061cfcce9e0a6a6
Anoop Elias <anoopelias@gmail.com>
2022-12-01 10:37:25 +0530

    Updated

--- src/2020/05/one.ts
+++ src/2020/05/one.ts
@@ -3,15 +3,19 @@ import { promises as fs } from 'fs';
 async function solve() {
     const input = await fs.readFile("src/2020/05/input");
     const splits = input.toString().split("\n");
+    console.log(splits.length)
     let max = 0;
+    let seats = new Map<number, number>();
+    let minRow = Number.MAX_SAFE_INTEGER;
+    let maxRow = 0;
 
     for (let split of splits) {
         let rowMin = 0;
         let rowMax = 127;
         for (let i = 0; i < 7; i++) {
-            console.log("Before row", rowMin, rowMax);
+            //console.log("Before row", rowMin, rowMax);
             [rowMin, rowMax] = partition(split.charAt(i), rowMin, rowMax);
-            console.log("After row", rowMin, rowMax);
+            //console.log("After row", rowMin, rowMax);
         }
 
         let colMin = 0;
@@ -26,9 +30,26 @@ async function solve() {
             console.log(rowMin, rowMax, colMin, colMax);
         const seat = (rowMin * 8) + colMin;
         max = Math.max(max, seat);
+        minRow = Math.min(minRow, rowMin);
+        maxRow = Math.max(maxRow, rowMin);
+
+        seats.set(seat, rowMin)
     }
 
-    console.log(max);
+    console.log(minRow, maxRow)
+    for (let seat of seats.keys()) {
+        if (seats.get(seat) !== minRow && seats.get(seat) !== maxRow) {
+            if (seats.has(seat + 2)) {
+                console.log(seat + 1)
+            }
+            if (seats.has(seat - 2)) {
+                console.log(seat - 1)
+            }
+
+        }
+        
+    }
+    //console.log(max);
 }
 
 function partition(letter: string, min: number, max: number) {
---
+++ src/2022/01/one.ts
@@ -0,0 +1,26 @@
+import { promises as fs } from 'fs';
+
+async function solve() {
+    const input = await fs.readFile("src/2022/01/input2");
+    const splits = input.toString().split("\n");
+    let hand = 0;
+    let max = 0;
+    const hands: number[] = [];
+
+
+    for (let i =0; i < splits.length; i++) {
+        if (splits[i].trim().length === 0) {
+            hands.push(hand);
+            max = Math.max(max, hand);
+            hand = 0;
+        } else {
+            hand += parseInt(splits[i])
+        }
+    }
+    hands.sort((a, b) => a - b);
+    console.log(hands.pop()! + hands.pop()! + hands.pop()!)
+    console.log(max)
+}
+
+console.log("Solving");
+solve();

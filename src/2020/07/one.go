f66ac8f546c71ea6f0892c94322a30b435e6df42
Anoop Elias <anoopelias@gmail.com>
2022-12-04 10:42:47 +0530

    Updated

--- src/2020/07/one.go
+++ src/2020/07/one.go
@@ -14,7 +14,7 @@ func main() {
 	fmt.Println("No of lines: " + strconv.Itoa(len(ls)))
 
 	g := make(map[string][]string)
-	rx, _ := regexp.Compile("([0-9]*) ([a-zA-Z ]*) bag(s)?(, ([0-9])* (.*)bag(s)?)?.")
+	rx, _ := regexp.Compile("([0-9]*) (.*?) bag(s)?(, ([0-9])* (.*) bag(s)?)?.")
 	for _, l := range ls {
 		splits := strings.Split(l, "bags contain")
 		bag := strings.Trim(splits[0], " ")
@@ -24,19 +24,72 @@ func main() {
 		}
 
 		if !strings.Contains(splits[1], "no other bags") {
-			bag1 := rx.FindStringSubmatch(splits[1])[2]
-			bag2 := rx.FindStringSubmatch(splits[1])[6]
+			bag1 := rx.FindStringSubmatch(splits[1])[1]
+			bag2 := rx.FindStringSubmatch(splits[1])[3]
 			if _, f := g[bag1]; !f {
 				g[bag1] = make([]string, 0)
 			}
 			if _, f := g[bag2]; !f {
 				g[bag2] = make([]string, 0)
 			}
+			if len(bag1) != 0 {
+				g[bag1] = []string{bag}
+			}
+			if len(bag2) != 0 {
+				g[bag2] = []string{bag}
+			}
 		}
 	}
+
+	bags := keysOf(g)
+	marked := make([]bool, len(bags))
+	queue := make([]string, 0)
+	queue = append(queue, "shiny gold")
+	marked[findBag("shiny gold", bags)] = true
+	var head string
+
+	for len(queue) != 0 {
+		head, queue = queue[0], queue[1:]
+		if !marked[findBag(head, bags)] {
+			covers := g[head]
+			marked[findBag(head, bags)] = true
+			for _, cover := range covers {
+				if !marked[findBag(cover, bags)] {
+					queue = append(queue, cover)
+				}
+			}
+
+		}
+	}
+	cnt := 0
+	for _, b := range marked {
+		if b {
+			cnt++
+		}
+	}
+
+	fmt.Println(cnt)
 }
 
-func setEdge()
+func findBag(b string, bags []string) int {
+	for i, bag := range bags {
+		if bag == b {
+			return i
+		}
+	}
+	return -1
+}
+
+func keysOf(m map[string][]string) []string {
+	keys := make([]string, len(m))
+
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+	return keys
+}
 
 func linesOf(fn string) []string {
 	fbyts, err := ioutil.ReadFile(fn)
--- src/2022/04/one.go
+++ src/2022/04/one.go
@@ -9,15 +9,26 @@ import (
 
 func main() {
 	fmt.Println("Starting...")
-	ls := linesOf("input")
+	ls := linesOf("input2")
 	p := 0
 
 	fmt.Println("No of lines: " + strconv.Itoa(len(ls)))
 	for _, l := range ls {
-		fmt.Println(l)
+		//fmt.Println(l)
+		splits := strings.Split(l, ",")
+		e1r := strings.Split(splits[0], "-")
+		e2r := strings.Split(splits[1], "-")
+		e1s, _ := strconv.Atoi(e1r[0])
+		e1e, _ := strconv.Atoi(e1r[1])
+		e2s, _ := strconv.Atoi(e2r[0])
+		e2e, _ := strconv.Atoi(e2r[1])
+
+		if (e1e < e2s) || (e2e < e1s) {
+			p++
+		}
 	}
 
-	fmt.Println(p)
+	fmt.Println(len(ls) - p)
 }
 
 func linesOf(fn string) []string {

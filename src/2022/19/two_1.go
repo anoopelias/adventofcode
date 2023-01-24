2ae5c369d4eaf8c1502296e543c0fedd1f3154fc
Anoop Elias <anoopelias@gmail.com>
2023-01-24 14:37:32 +0530

    Updated

--- src/2022/19/two_1.go
+++ src/2022/19/two_1.go
@@ -66,7 +66,7 @@ type result struct {
 func main() {
 	fmt.Println("Starting...")
 	ls := linesOf("input1")
-	time := 32
+	time := 24
 
 	bps := make([]blueprint, 0)
 	for _, l := range ls {
@@ -94,71 +94,92 @@ func main() {
 		})
 	}
 
-	out := 1
-	tysnow := make([]int, time)
-	for i := 0; i < len(tysnow); i++ {
-		tysnow[i] = -1
-	}
+	// out := 1
+	// for _, bp := range bps {
+	// 	bu := builder{
+	// 		bp:   bp,
+	// 		rbs:  []int{1, 0, 0, 0},
+	// 		maxt: time,
+	// 	}
+	// 	memo := map[string]result{}
+	// 	res := bu.maxGeode(0, newItems(), &memo, []int{})
+	// 	max := res.max
+	// 	fmt.Printf("bp: %d: max :%d\n", bp.n, max)
+	// 	printMaxtys(res.maxtys, bp)
+	// 	out *= max
+	// }
 
-	for _, bp := range bps {
-		bu := builder{
-			bp:   bp,
-			rbs:  []int{1, 0, 0, 0},
-			maxt: time,
-		}
-		memo := map[string]int{}
-		//tysnow := make([]int, time)
-		max := bu.maxGeode(0, newItems(), &memo)
-		fmt.Printf("bp: %d: max :%d\n", bp.n, max)
-		//printMaxtys(tysnow, bp)
-		out *= max
-	}
+	// fmt.Println(out)
 
-	fmt.Println(out)
+	bu := builder{
+		bp:   bps[0],
+		rbs:  []int{1, 4, 2, 1},
+		maxt: time,
+	}
+	memo := map[string]result{}
+	its := items{
+		cnt: []int{2, 17, 3, 0},
+	}
+	tysnow := []int{-1, -1, 2, -1, -1, 3}
+	bu.maxGeode(18, its, &memo, tysnow)
 }
 
 func newItems() items {
 	return items{cnt: []int{0, 0, 0, 0}}
 }
 
-func (b *builder) maxGeode(min int, its items, memo *map[string]int) int {
+func (b *builder) maxGeode(min int, its items, memo *map[string]result, tysnow []int) result {
 	if min == b.maxt {
-		return its.cnt[3]
+		return result{its.cnt[3], append([]int{}, tysnow...)}
 	}
 
-	maxx := 0
+	maxx := result{}
 	h := b.hash(min, its)
 
 	if v, fnd := (*memo)[h]; fnd {
+		//fmt.Printf("depth%s: min %d memoized\n", depth, min)
 		return v
 	}
 
-	is := []int{0, 1, 2, 3}
+	is := []int{0, 3}
 
 	// no robots
 	ms := b.maxt - min
 	nrmax := (b.rbs[GEO] * ms) + its.cnt[GEO]
-	if nrmax > maxx {
-		maxx = nrmax
+	if nrmax > maxx.max {
+		maxx = result{
+			max:    nrmax,
+			maxtys: append(tysnow, getTys(ms)...),
+		}
 	}
+	fmt.Printf("%d beginning: %s %d %v\n", min, h, maxx.max, maxx.maxtys)
 	for _, ty := range is {
 		ms := b.timeToTy(its, ty)
 		if ms > 0 && ms < b.maxt-min {
 			nits := its.next(b.rbs, ms)
 			nits = nits.make(b.bp, ty)
-			nmin := min + ms
 			b.rbs[ty]++
-			//(*tysnow)[nmin-1] = ty
-			maxgeo := b.maxGeode(nmin, nits, memo)
-			//(*tysnow)[nmin-1] = -1
+			//fmt.Printf("ty: %d ms: %d\n", ty, ms)
+			fmt.Printf("%d_%d before1 %d %d %v\n", min, ty, min+ms, maxx.max, maxx.maxtys)
+			tysTy := getTysTy(ms, ty)
+			fmt.Printf("%d_%d before2 %d %d %v\n", min, ty, min+ms, maxx.max, maxx.maxtys)
+			ntysnow := append(tysnow, tysTy...)
+			fmt.Printf("%d_%d before3 %d %d %v\n", min, ty, min+ms, maxx.max, maxx.maxtys)
+			maxgeo := b.maxGeode(min+ms, nits, memo, ntysnow)
+			fmt.Printf("%d_%d after %d %d %v\n", min, ty, maxx.max, maxx.maxtys, maxgeo.max)
 			b.rbs[ty]--
-			if maxgeo > maxx {
-				maxx = maxgeo
+			if maxgeo.max > maxx.max {
+				fmt.Printf("%d_%d updating %d %d %v\n", min, ty, maxgeo.max, maxx.max, maxx.maxtys)
+				maxx = result{
+					max:    maxgeo.max,
+					maxtys: append([]int{}, maxgeo.maxtys...),
+				}
 			}
 		}
 	}
 
-	(*memo)[h] = maxx
+	fmt.Printf("%d end: %d %v\n", min, maxx.max, maxx.maxtys)
+	//(*memo)[h] = max
 	return maxx
 }
 
@@ -183,6 +204,23 @@ func (b *builder) timeToTy(its items, ty int) int {
 	return time + 1
 }
 
+func getTys(ms int) []int {
+	tys := []int{}
+	for i := 0; i < ms; i++ {
+		tys = append(tys, -1)
+	}
+	return tys
+}
+
+func getTysTy(ms int, ty int) []int {
+	tys := []int{}
+	for i := 0; i < ms-1; i++ {
+		tys = append(tys, -1)
+	}
+	tys = append(tys, ty)
+	return tys
+}
+
 func (b *builder) add(tys []int) {
 	for _, ty := range tys {
 		b.rbs[ty]++
@@ -193,12 +231,6 @@ func (b *builder) reset(tys []int) {
 	for _, ty := range tys {
 		b.rbs[ty]--
 	}
-}
-
-func sliceCopy(s []int) []int {
-	c := make([]int, len(s))
-	copy(c, s)
-	return c
 }
 
 func sliceAdd(a []int, b []int) (res []int) {

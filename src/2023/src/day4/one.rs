bb4983af930712acde3467975cb8ae7c4c1360c0
Anoop Elias <anoopelias@gmail.com>
2023-12-04 16:26:32 +0530

    Convert `one` to fp

--- src/2023/src/day4/one.rs
+++ src/2023/src/day4/one.rs
@@ -1,16 +1,14 @@
 use crate::day4::wins_for;
 
 pub(crate) fn solve(lines: Vec<String>) -> String {
-    let mut sum = 0;
-    for line in lines.iter() {
-        let wins = wins_for(line);
-
-        let num: i32 = 2;
-        if wins > 0 {
-            sum += num.pow((wins - 1).try_into().unwrap());
-        }
-    }
-    sum.to_string()
+    lines
+        .iter()
+        .map(|line| wins_for(line))
+        .filter(|wins| wins > &0)
+        .map(|wins| 2i32.pow(wins as u32 - 1))
+        .reduce(|a, b| a + b)
+        .unwrap()
+        .to_string()
 }
 
 #[cfg(test)]

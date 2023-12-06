069e99ae0279a38f71209c8f9c49ae22ea092492
Anoop Elias <anoopelias@gmail.com>
2023-12-06 14:08:56 +0530

    Remove simple string_to_nums

--- src/2023/src/utils/parser.rs
+++ src/2023/src/utils/parser.rs
@@ -1,11 +1,16 @@
-use super::util::string_to_nums;
-
 pub trait I32Parser {
     fn parse_i32(&self) -> Vec<i32>;
 }
 
 impl I32Parser for &str {
     fn parse_i32(&self) -> Vec<i32> {
-        string_to_nums(self)
+        self.trim()
+            .split(" ")
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|s| s.trim())
+            .filter(|s| s.len() > 0)
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect()
     }
 }

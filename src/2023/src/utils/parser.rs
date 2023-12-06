da5b863d2f035e2f77d2bf0f99b2dcb6cb217048
Anoop Elias <anoopelias@gmail.com>
2023-12-06 14:30:12 +0530

    Extract space parser

--- src/2023/src/utils/parser.rs
+++ src/2023/src/utils/parser.rs
@@ -2,15 +2,28 @@ pub trait I32Parser {
     fn parse_i32(self) -> Vec<i32>;
 }
 
+pub trait SpaceParser {
+    fn parse_space(self) -> Vec<String>;
+}
+
 impl I32Parser for &str {
     fn parse_i32(self) -> Vec<i32> {
+        self.parse_space()
+            .iter()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect()
+    }
+}
+
+impl SpaceParser for &str {
+    fn parse_space(self) -> Vec<String> {
         self.trim()
             .split(" ")
             .collect::<Vec<_>>()
             .iter()
             .map(|s| s.trim())
             .filter(|s| s.len() > 0)
-            .map(|s| s.parse::<i32>().unwrap())
+            .map(|s| s.to_string())
             .collect()
     }
 }

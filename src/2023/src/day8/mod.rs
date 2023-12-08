13700ee175f8ac7a3a1f283725f4ff1a84ab90fe
Anoop Elias <anoopelias@gmail.com>
2023-12-08 13:26:32 +0530

    Use reference while iterating

--- src/2023/src/day8/mod.rs
+++ src/2023/src/day8/mod.rs
@@ -32,10 +32,8 @@ trait Solution {
 impl Problem {
     fn parse(&self) -> (Vec<char>, HashMap<String, (String, String)>) {
         let mut map: HashMap<String, (String, String)> = HashMap::new();
-        let mut lines = self.lines.clone();
-        let instr: Vec<char> = lines.remove(0).chars().collect();
-        lines.remove(0);
-        for line in lines {
+        let instr: Vec<char> = self.lines.get(0).unwrap().chars().collect();
+        for line in self.lines.iter().skip(2) {
             let splits = line.parse_sep("=");
             let key = splits.get(0).unwrap().to_string();
             let mut values = splits.get(1).unwrap().remove_wrapping().parse_sep(", ");

0e20ee0334c5886a1b99224dd0c867254e285aeb
Anoop Elias <anoopelias@gmail.com>
2023-12-08 13:40:53 +0530

    Avoid using

--- src/2023/src/day8/mod.rs
+++ src/2023/src/day8/mod.rs
@@ -34,9 +34,14 @@ impl Problem {
         let mut map: HashMap<&str, (&str, &str)> = HashMap::new();
         let instr: Vec<char> = self.lines.get(0).unwrap().chars().collect();
         for line in self.lines.iter().skip(2) {
-            let splits = line.parse_sep("=");
-            let key = splits.get(0).unwrap();
-            let mut values = splits.get(1).unwrap().remove_wrapping().parse_sep(", ");
+            let mut splits = line.split("=");
+            let key = splits.next().unwrap().trim();
+            let mut values = splits
+                .next()
+                .unwrap()
+                .trim()
+                .remove_wrapping()
+                .parse_sep(", ");
             map.insert(key, (values.remove(0), values.remove(0)));
         }
         (instr, map)

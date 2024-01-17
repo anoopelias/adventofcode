414511c5186c17bdc0feebd96bc863872c9fb80f
Anoop Elias <anoopelias@gmail.com>
2024-01-17 08:38:42 +0530

    Day 21 WIP

--- src/2023/src/day21/mod.rs
+++ src/2023/src/day21/mod.rs
@@ -2,6 +2,7 @@ const DAY: &str = "day21";
 
 use std::{
     collections::{HashMap, HashSet, VecDeque},
+    hash,
     time::Instant,
 };
 
@@ -18,7 +19,7 @@ pub(crate) fn solve() -> String {
     let elapsed1 = time.elapsed();
 
     let time = Instant::now();
-    let part2 = part2(&lines);
+    let part2 = part2(&lines, 26501365);
     let elapsed2 = time.elapsed();
     return format!(
         "result1: {} in {:?} \nresult2: {} in {:?}",
@@ -37,8 +38,52 @@ fn part1(lines: &Vec<String>) -> String {
     .to_string()
 }
 
-fn part2(lines: &Vec<String>) -> String {
-    "".to_string()
+fn part2(lines: &Vec<String>, steps: usize) -> String {
+    let grid = lines.to_grid();
+    let dist_map = grid.bfs(grid.find('S').unwrap(), |neighbor| {
+        neighbor.cell.val == &'.' || neighbor.cell.val == &'S'
+    });
+
+    let width = grid.m;
+    let half_width = width / 2;
+    let n = (steps - half_width) / width;
+
+    assert_eq!(steps, (n * width) + half_width);
+    let mut evens = dist_map
+        .values()
+        .filter(|&&value| value % 2 == 0)
+        .collect::<Vec<_>>()
+        .len();
+
+    let mut odds = dist_map
+        .values()
+        .filter(|&&value| value % 2 == 1)
+        .collect::<Vec<_>>()
+        .len();
+
+    let mut odd_cuts = dist_map
+        .values()
+        .filter(|&&value| value % 2 == 1 && value as usize > half_width)
+        .collect::<Vec<_>>()
+        .len();
+
+    let mut even_cuts = dist_map
+        .values()
+        .filter(|&&value| value % 2 == 0 && value as usize > half_width)
+        .collect::<Vec<_>>()
+        .len();
+
+    if steps % 2 == 1 {
+        (odds, evens) = (evens, odds);
+        (odd_cuts, even_cuts) = (even_cuts, odd_cuts);
+    }
+
+    let n_sq = n * n;
+    let np1 = n + 1;
+    let np1_sq = np1 * np1;
+
+    let result = (evens * np1_sq) + (odds * n_sq) - (np1 * even_cuts) + (n * odd_cuts);
+    result.to_string()
 }
 
 #[cfg(test)]
@@ -60,13 +105,13 @@ mod tests {
 
     #[test]
     fn test_part2_sample() {
-        // let lines = util::lines_in(&format!("./src/{}/input", DAY));
-        // assert_eq!("167409079868000", part2(&lines));
+        let lines = util::lines_in(&format!("./src/{}/input", DAY));
+        assert_eq!("", part2(&lines, 500));
     }
 
     #[test]
     fn test_part2_input() {
-        // let lines = util::lines_in(&format!("./src/{}/input1", DAY));
-        // assert_eq!("131029523269531", part2(&lines));
+        let lines = util::lines_in(&format!("./src/{}/input1", DAY));
+        assert_eq!("597102954104491", part2(&lines, 26501365));
     }
 }

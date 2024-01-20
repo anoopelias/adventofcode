c63471a78c32a0afcd14ef7f96ee58c57b03fa12
Anoop Elias <anoopelias@gmail.com>
2024-01-21 01:49:10 +0530

    Day 22 part 2 complete

--- src/2023/src/day22/mod.rs
+++ src/2023/src/day22/mod.rs
@@ -1,12 +1,16 @@
 const DAY: &str = "day22";
 
 use std::{
+    cmp,
     collections::{HashMap, HashSet},
     str::FromStr,
     time::Instant,
 };
 
-use crate::utils::grid3d::Coord3d;
+use crate::utils::{
+    grid3d::Coord3d,
+    pq::{self, Pq},
+};
 use anyhow::Error;
 
 use crate::utils::util;
@@ -87,34 +91,24 @@ impl Brick {
             .filter(|box_num| *box_num != num)
             .collect::<HashSet<_>>()
     }
+
+    fn top(&self) -> u32 {
+        self.coords
+            .iter()
+            .fold(0, |top, coord| cmp::max(top, coord.r))
+    }
 }
 
 fn part1(lines: &Vec<String>) -> String {
     let mut bricks = parse_lines(lines);
+    let mut coords = prepare_coords(&bricks);
+    stabilize(&mut bricks, &mut coords);
+    let supporting_bricks = supporting_bricks(&bricks, coords);
 
-    let mut coords: HashMap<Coord3d, usize> = bricks
-        .iter()
-        .enumerate()
-        .flat_map(|(n, brick)| {
-            brick
-                .coords
-                .iter()
-                .map(|coord| (*coord, n))
-                .collect::<Vec<_>>()
-        })
-        .collect();
+    (bricks.len() - supporting_bricks.len()).to_string()
+}
 
-    let mut tick = true;
-    while tick {
-        tick = bricks.iter_mut().fold(false, |tick, brick| {
-            if brick.can_go_down(&coords) {
-                brick.go_down(&mut coords);
-                return true;
-            }
-            tick
-        });
-    }
-
+fn supporting_bricks(bricks: &Vec<Brick>, coords: HashMap<Coord3d, usize>) -> HashSet<usize> {
     let supporting_bricks = bricks
         .iter()
         .map(|brick| {
@@ -128,8 +122,35 @@ fn part1(lines: &Vec<String>) -> String {
         .filter(|s| s.is_some())
         .map(|s| s.unwrap())
         .collect::<HashSet<usize>>();
+    supporting_bricks
+}
 
-    (bricks.len() - supporting_bricks.len()).to_string()
+fn stabilize(bricks: &mut Vec<Brick>, coords: &mut HashMap<Coord3d, usize>) {
+    let mut tick = true;
+    while tick {
+        tick = bricks.iter_mut().fold(false, |tick, brick| {
+            if brick.can_go_down(&*coords) {
+                brick.go_down(coords);
+                return true;
+            }
+            tick
+        });
+    }
+}
+
+fn prepare_coords(bricks: &Vec<Brick>) -> HashMap<Coord3d, usize> {
+    let coords: HashMap<Coord3d, usize> = bricks
+        .iter()
+        .enumerate()
+        .flat_map(|(n, brick)| {
+            brick
+                .coords
+                .iter()
+                .map(|coord| (*coord, n))
+                .collect::<Vec<_>>()
+        })
+        .collect();
+    coords
 }
 
 fn parse_lines(lines: &Vec<String>) -> Vec<Brick> {
@@ -139,37 +160,135 @@ fn parse_lines(lines: &Vec<String>) -> Vec<Brick> {
         .collect::<Vec<_>>()
 }
 
-fn part2(_lines: &Vec<String>) -> String {
-    "".to_string()
+struct PqNode {
+    brick_num: usize,
+    weight: u32,
+}
+
+impl Ord for PqNode {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+impl PartialOrd for PqNode {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        self.weight.partial_cmp(&other.weight)
+    }
+}
+
+impl PartialEq for PqNode {
+    fn eq(&self, other: &Self) -> bool {
+        self.weight == other.weight
+    }
+}
+
+impl Eq for PqNode {}
+
+fn part2(lines: &Vec<String>) -> String {
+    let mut bricks = parse_lines(lines);
+    let mut coords = prepare_coords(&bricks);
+    stabilize(&mut bricks, &mut coords);
+
+    let by_bricks_map = bricks
+        .iter()
+        .map(|brick| brick.supported_by(&coords))
+        .collect::<Vec<HashSet<usize>>>();
+
+    let to_brick_map = by_bricks_map.iter().enumerate().fold(
+        HashMap::new(),
+        |mut map: HashMap<usize, HashSet<usize>>, (i, by_bricks)| {
+            by_bricks.iter().for_each(|by| match map.get_mut(by) {
+                Some(set) => {
+                    set.insert(i);
+                }
+                None => {
+                    let mut set = HashSet::new();
+                    set.insert(i);
+                    map.insert(*by, set);
+                }
+            });
+            map
+        },
+    );
+
+    let supporting_bricks = supporting_bricks(&bricks, coords);
+    let mut sum = 0;
+
+    for brick_num in supporting_bricks {
+        let mut pq = Pq::new(pq::PqType::Min);
+        let mut disinteg_bricks = HashSet::new();
+        disinteg_bricks.insert(brick_num);
+
+        to_brick_map
+            .get(&brick_num)
+            .unwrap()
+            .iter()
+            .for_each(|to_brick| {
+                let node = PqNode {
+                    brick_num: *to_brick,
+                    weight: bricks.get(*to_brick).unwrap().top(),
+                };
+                pq.push(node);
+            });
+
+        while !pq.is_empty() {
+            let node = pq.pop().unwrap();
+            let can_disintegrate = by_bricks_map
+                .get(node.brick_num)
+                .unwrap()
+                .iter()
+                .all(|by_brick| disinteg_bricks.contains(by_brick));
+
+            if can_disintegrate {
+                disinteg_bricks.insert(node.brick_num);
+                match to_brick_map.get(&node.brick_num) {
+                    Some(to_bricks) => to_bricks.iter().for_each(|to_brick| {
+                        pq.push(PqNode {
+                            brick_num: *to_brick,
+                            weight: bricks.get(*to_brick).unwrap().top(),
+                        })
+                    }),
+                    None => {}
+                }
+            }
+        }
+
+        sum += disinteg_bricks.len() - 1;
+    }
+
+    sum.to_string()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{part1, DAY};
+    use clearcheck::assertions::equal::EqualityAssertion;
+
+    use super::{part1, part2, DAY};
     use crate::utils::util;
 
     #[test]
     fn test_part1_sample() {
         let lines = util::lines_in(&format!("./src/{}/input", DAY));
-        assert_eq!("5", part1(&lines))
+        part1(&lines).should_equal("5");
     }
 
     #[test]
     fn test_part1_input() {
         let lines = util::lines_in(&format!("./src/{}/input1", DAY));
-        // 660 too high
-        assert_eq!("463", part1(&lines))
+        part1(&lines).should_equal("463");
     }
 
     #[test]
     fn test_part2_sample() {
-        // let lines = util::lines_in(&format!("./src/{}/input", DAY));
-        // assert_eq!("145", part2(&lines))
+        let lines = util::lines_in(&format!("./src/{}/input", DAY));
+        part2(&lines).should_equal("7");
     }
 
     #[test]
     fn test_part2_input() {
-        // let lines = util::lines_in(&format!("./src/{}/input1", DAY));
-        // assert_eq!("259356", part2(&lines))
+        let _lines = util::lines_in(&format!("./src/{}/input1", DAY));
+        // Too slow to test every time
+        part2(&_lines).should_equal("89727");
     }
 }
